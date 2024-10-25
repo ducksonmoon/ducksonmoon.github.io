393e4b4e76ef0280f4c532006d3ec9a91f9c1f3a
Mehrshad Baqerzadegan <MehrshadBaqerzadegan@gmail.com>
2024-10-25 13:32:10 +0330

    Add custom favicon: Rocket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+
       <body className="flex flex-col min-h-screen pt-16 bg-background text-foreground">
         <Navbar />
         <main className="flex-grow">{children}</main>
