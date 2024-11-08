a149583179d6aeadb957731598206ae06ecab592
Mehrshad Baqerzadegan <MehrshadBaqerzadegan@gmail.com>
2024-11-08 17:02:11 +0330

    Fix: remove unused

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { FaBriefcase, FaGlobe, FaGithub, FaCode } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
 import Image from "next/image";
 
 // const experiences = [
